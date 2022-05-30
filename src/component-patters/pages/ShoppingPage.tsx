85c12e6c58253c890ff19d96e8e638aeed1dab4c
Javier Villanueva <javier.villanueva@rookmotion.com>
2022-05-30 12:29:13 -0500

    :truck: preparing for publish

--- src/component-patters/pages/ShoppingPage.tsx
+++ src/component-patters/pages/ShoppingPage.tsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-
 // Components
 import { ProductCard } from '../components'
-
-// Estilos
-import '../styles/custom-styles.css'
 
 // DataSource
 import { products } from '../data/products'
@@ -18,34 +13,16 @@ export const ShoppingPage = () => {
 
         <ProductCard 
           product = { products[0] }
-          className='bg-dark text-white'
           initialValues={{
             count: 4,
             maxCount: 10,
           }}
         >
-          { ({ isMaxCountReached, reset, increaseBy }) => (
+          { () => (
             <>
-              <ProductCard.Image className='custom-image' />
-              <ProductCard.Title className = 'text-white text-bold' />
-              <ProductCard.Buttons className = 'custom-buttons' />
-
-              <button onClick = { reset }>
-                Reset
-              </button>
-
-              <button onClick={() => increaseBy(-2)}>
-                -2
-              </button>
-
-              <button 
-                onClick={() => increaseBy(2)}
-                style = {{
-                  display: isMaxCountReached ? 'none' : 'inline-block'
-                }}
-              >
-                +2
-              </button>
+              <ProductCard.Image />
+              <ProductCard.Title />
+              <ProductCard.Buttons />
             </>
           )}
         </ProductCard>
